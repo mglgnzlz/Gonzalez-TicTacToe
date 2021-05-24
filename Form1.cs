9d343d96279b79878eb7a8b5f0fa59c3b12a7099
Miguel <mglgnzlz13@gmail.com>
2021-05-24 11:58:46 +0800

    Made a win checking algorithm for a 3x3 grid tic tac toe game.

--- Form1.cs
+++ Form1.cs
@@ -61,5 +61,73 @@ namespace Gonzalez_TicTacToe
         {
 
         }
+
+        //
+        //Once executed, the function checks if one of the following conditions is met
+        //
+
+        public void CheckWin_Algo()
+        {
+            bool MessageWinner = false;
+
+            //
+            //Horizontal Patterns
+            //
+
+            // A1 = A2 = A3 & A1 != " "
+            if ((Btn_A1.Text == Btn_A2.Text) && (Btn_A2.Text == Btn_A3.Text) && (Btn_A1.Text != " "))
+            {
+                MessageWinner = true;
+            }
+
+            // B1 = B2 = B3 & B1 != " "
+            else if ((Btn_B1.Text == Btn_B2.Text) && (Btn_B2.Text == Btn_B3.Text) && (Btn_B1.Text != " "))
+            {
+                MessageWinner = true;
+            }
+
+            // C1 = C2 = C3 & C1 != " "
+            else if ((Btn_C1.Text == Btn_C2.Text) && (Btn_C2.Text == Btn_C3.Text) && (Btn_C1.Text != " "))
+            {
+                MessageWinner = true;
+            }
+
+            //
+            //Vertical Patterns
+            //
+
+            // A1 = B1 = C1 & A1 != " "
+            else if ((Btn_A1.Text == Btn_B1.Text) && (Btn_C1.Text == Btn_C1.Text) && (Btn_B1.Text != " "))
+            {
+                MessageWinner = true;
+            }
+
+            // A2 = B2 = C3 & A2 != " "
+            else if ((Btn_A2.Text == Btn_B2.Text) && (Btn_B2.Text == Btn_C2.Text) && (Btn_A2.Text != " "))
+            {
+                MessageWinner = true;
+            }
+
+            // A3 = B3 = C3 & A3 != " "
+            else if ((Btn_A3.Text == Btn_B3.Text) && (Btn_B3.Text == Btn_C3.Text) && (Btn_A3.Text != " "))
+            {
+                MessageWinner = true;
+            }
+
+            //Diagonal Patterns
+
+            // A1 = B2 = C3 & A1 != " "
+            else if ((Btn_A1.Text == Btn_B2.Text) && (Btn_B2.Text == Btn_C3.Text) && (Btn_A1.Text != " "))
+            {
+                MessageWinner = true;
+            }
+
+            // A3 = B2 = C1 & A3 != " "
+            else if ((Btn_A3.Text == Btn_B2.Text) && (Btn_B2.Text == Btn_C1.Text) && (Btn_A3.Text != " "))
+            {
+                MessageWinner = true;
+            }
+        }
+
     }
 }
